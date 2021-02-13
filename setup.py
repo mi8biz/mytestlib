bbffb4b7cb071041efd2b596d5ffc2fdc1ee6751
mi8biz <68505376+mi8biz@users.noreply.github.com>
2021-02-14 00:44:49 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='mytestlib',
+    version='1.0.0',
+    url='',
+    author='mi8biz',
+    description='My test library.',
+    packages=find_packages())
+
